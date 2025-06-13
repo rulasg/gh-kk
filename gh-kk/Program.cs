6b9436bb107dc40171da03b9512f4bc21ffa6fe8
rulasg <rulasg@github.com>
2025-06-13 15:09:55 +0200

    refactor: reorganize Main method and improve global options handling

--- gh-kk/Program.cs
+++ gh-kk/Program.cs
@@ -9,67 +9,70 @@ REFERENCE: https://learn.microsoft.com/en-us/dotnet/standard/commandline/
 using System.Collections;
 using System.CommandLine;
 using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using gh_kk.Commands;
 
-namespace gh_kk;
+[assembly: InternalsVisibleTo("gh-kk.test")]
+namespace gh_kk{
 
-class Program
-{
-
-    static Hashtable globalOptions = new Hashtable();
-
-    static async Task<int> Main(string[] args)
+    internal class Program
     {
 
-        var rootCommand = new RootCommand("Sample app for System.CommandLine"); // No action if no parameter is provided
+        static Hashtable globalOptions = new Hashtable();
 
-        var verboseOption = new Option<bool>(
-            aliases: new[] { "--verbose", "-v" },
-            description: "Enable verbose output (global)");
-        verboseOption.AddAlias("-v");
-
-        rootCommand.AddGlobalOption(verboseOption);
-        GlobalOptions.AddOption("verbose", verboseOption);
-
-        rootCommand
-            .AddSubCommand1()
-            .AddSubCommand2()
-            .AddGetTokenCommand();
-
-        if (args.Length == 0)
-            return await rootCommand.InvokeAsync("--help");
-
-        return await rootCommand.InvokeAsync(args);
-    }
-
-}
-
-static class GlobalOptions
-{
-    static Hashtable options;
-
-    static GlobalOptions()
-    {
-        options = new Hashtable();
-    }
-
-    public static void AddOption(string name, Option option)
-    {
-        if (!options.ContainsKey(name))
+        public static async Task<int> Main(string[] args)
         {
-            options.Add(name, option);
-        }
-    }
 
-    public static Option<T> GetOption<T>(string name)
-    {
-        Option<T>? optionValue = options[name] as Option<T>;
+            var rootCommand = new RootCommand("Sample app for System.CommandLine"); // No action if no parameter is provided
 
-        if (optionValue == null)
-        {
-            throw new ArgumentException($"Option {name} is not of type Option<{typeof(T).Name}>");
+            var verboseOption = new Option<bool>(
+                aliases: new[] { "--verbose", "-v" },
+                description: "Enable verbose output (global)");
+            verboseOption.AddAlias("-v");
+
+            rootCommand.AddGlobalOption(verboseOption);
+            GlobalOptions.AddOption("verbose", verboseOption);
+
+            rootCommand
+                .AddSubCommand1()
+                .AddSubCommand2()
+                .AddGetTokenCommand();
+
+            if (args.Length == 0)
+                return await rootCommand.InvokeAsync("--help");
+
+            return await rootCommand.InvokeAsync(args);
         }
 
-        return optionValue;
+    }
+
+    static class GlobalOptions
+    {
+        static Hashtable options;
+
+        static GlobalOptions()
+        {
+            options = new Hashtable();
+        }
+
+        public static void AddOption(string name, Option option)
+        {
+            if (!options.ContainsKey(name))
+            {
+                options.Add(name, option);
+            }
+        }
+
+        public static Option<T> GetOption<T>(string name)
+        {
+            Option<T>? optionValue = options[name] as Option<T>;
+
+            if (optionValue == null)
+            {
+                throw new ArgumentException($"Option {name} is not of type Option<{typeof(T).Name}>");
+            }
+
+            return optionValue;
+        }
     }
 }
