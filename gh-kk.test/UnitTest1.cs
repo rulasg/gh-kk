d3dad3aeead183e2dbc28b80a1841f71545ce1da
rulasg <rulasg@github.com>
2025-06-13 15:10:18 +0200

    test: add cmdTest utility for capturing console output

--- gh-kk.test/UnitTest1.cs
+++ gh-kk.test/UnitTest1.cs
@@ -1,10 +1,24 @@
-﻿namespace gh_kk.test;
+﻿using System.IO;
+using System.Text;
+using Xunit;
+
+
+namespace gh_kk.test;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        // Arrange
+        var args = new string[] { "--version" };
 
+        // Act
+        var result = cmdTest.RunAndGetConsoleOutput(args);
+
+        // Assert
+        Assert.Contains("17.11.1", result);
     }
+
+
 }
